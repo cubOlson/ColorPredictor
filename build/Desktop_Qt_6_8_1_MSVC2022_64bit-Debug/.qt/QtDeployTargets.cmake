a014ed335f08e8e1f02a80b4559d148203dcbe53
Cub Olson <cub.olson@gmail.com>
2025-02-27 13:34:58 -0700

    added build to gitignore

--- build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ColorPredictionApp_FILE C:/Users/Lyman/Documents/ColorPredictionApp/build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/ColorPredictionApp.exe)
-set(__QT_DEPLOY_TARGET_ColorPredictionApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ColorPredictionApp_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
